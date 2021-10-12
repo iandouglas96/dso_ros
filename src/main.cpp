98b388660ea21982f9d62032021359ced3195d0b
Ian D. Miller <iandouglas96@gmail.com>
2021-10-12 16:17:30 -0400

    Loading ROS params

--- src/main.cpp
+++ src/main.cpp
@@ -45,100 +45,7 @@
 #include <geometry_msgs/PoseStamped.h>
 #include "cv_bridge/cv_bridge.h"
 
-
-std::string calib = "";
-std::string vignetteFile = "";
-std::string gammaFile = "";
-std::string saveFile = "";
-bool useSampleOutput=false;
-
 using namespace dso;
-
-void parseArgument(char* arg)
-{
-	int option;
-	char buf[1000];
-	if(1==sscanf(arg,"savefile=%s",buf))
-	{
-		saveFile = buf;
-		printf("saving to %s on finish!\n", saveFile.c_str());
-		return;
-	}
-
-	if(1==sscanf(arg,"sampleoutput=%d",&option))
-	{
-		if(option==1)
-		{
-			useSampleOutput = true;
-			printf("USING SAMPLE OUTPUT WRAPPER!\n");
-		}
-		return;
-	}
-
-	if(1==sscanf(arg,"quiet=%d",&option))
-	{
-		if(option==1)
-		{
-			setting_debugout_runquiet = true;
-			printf("QUIET MODE, I'll shut up!\n");
-		}
-		return;
-	}
-
-
-	if(1==sscanf(arg,"nolog=%d",&option))
-	{
-		if(option==1)
-		{
-			setting_logStuff = false;
-			printf("DISABLE LOGGING!\n");
-		}
-		return;
-	}
-
-	if(1==sscanf(arg,"nogui=%d",&option))
-	{
-		if(option==1)
-		{
-			disableAllDisplay = true;
-			printf("NO GUI!\n");
-		}
-		return;
-	}
-	if(1==sscanf(arg,"nomt=%d",&option))
-	{
-		if(option==1)
-		{
-			multiThreading = false;
-			printf("NO MultiThreading!\n");
-		}
-		return;
-	}
-	if(1==sscanf(arg,"calib=%s",buf))
-	{
-		calib = buf;
-		printf("loading calibration from %s!\n", calib.c_str());
-		return;
-	}
-	if(1==sscanf(arg,"vignette=%s",buf))
-	{
-		vignetteFile = buf;
-		printf("loading vignette from %s!\n", vignetteFile.c_str());
-		return;
-	}
-
-	if(1==sscanf(arg,"gamma=%s",buf))
-	{
-		gammaFile = buf;
-		printf("loading gammaCalib from %s!\n", gammaFile.c_str());
-		return;
-	}
-
-	printf("could not parse argument \"%s\"!!\n", arg);
-}
-
-
-
 
 FullSystem* fullSystem = 0;
 Undistort* undistorter = 0;
@@ -177,70 +84,66 @@ void vidCb(const sensor_msgs::ImageConstPtr img)
 
 
 
-int main( int argc, char** argv )
+int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "dso_live");
-
-
-
-	for(int i=1; i<argc;i++) parseArgument(argv[i]);
-
+  ros::NodeHandle nh("~");
 
 	setting_desiredImmatureDensity = 1000;
 	setting_desiredPointDensity = 1200;
 	setting_minFrames = 5;
 	setting_maxFrames = 7;
-	setting_maxOptIterations=4;
-	setting_minOptIterations=1;
+	setting_maxOptIterations = 4;
+	setting_minOptIterations = 1;
 	setting_logStuff = false;
 	setting_kfGlobalWeight = 1.3;
 
+  // Load parameters from ROS
+  std::string calib = "";
+  bool showDisp = false;
+  nh.getParam("calib", calib);
+  nh.getParam("display", showDisp);
 
-	printf("MODE WITH CALIBRATION, but without exposure times!\n");
-	setting_photometricCalibration = 2;
+	ROS_INFO_STREAM("MODE WITHOUT CALIBRATION, without exposure times!\n");
+	setting_photometricCalibration = 0;
 	setting_affineOptModeA = 0;
 	setting_affineOptModeB = 0;
 
+  undistorter = Undistort::getUndistorterForFile(calib, "", "");
+
+  setGlobalCalib(
+          (int)undistorter->getSize()[0],
+          (int)undistorter->getSize()[1],
+          undistorter->getK().cast<float>());
 
 
-    undistorter = Undistort::getUndistorterForFile(calib, gammaFile, vignetteFile);
-
-    setGlobalCalib(
-            (int)undistorter->getSize()[0],
-            (int)undistorter->getSize()[1],
-            undistorter->getK().cast<float>());
+  fullSystem = new FullSystem();
+  fullSystem->linearizeOperation=false;
 
 
-    fullSystem = new FullSystem();
-    fullSystem->linearizeOperation=false;
+  if(showDisp)
+	  fullSystem->outputWrapper.push_back(new IOWrap::PangolinDSOViewer(
+	  		 (int)undistorter->getSize()[0],
+	  		 (int)undistorter->getSize()[1]));
 
 
-    if(!disableAllDisplay)
-	    fullSystem->outputWrapper.push_back(new IOWrap::PangolinDSOViewer(
-	    		 (int)undistorter->getSize()[0],
-	    		 (int)undistorter->getSize()[1]));
+  //fullSystem->outputWrapper.push_back(new IOWrap::SampleOutputWrapper());
 
 
-    if(useSampleOutput)
-        fullSystem->outputWrapper.push_back(new IOWrap::SampleOutputWrapper());
+  if(undistorter->photometricUndist != 0)
+  	fullSystem->setGammaFunction(undistorter->photometricUndist->getG());
 
+  ros::Subscriber imgSub = nh.subscribe("image", 1, &vidCb);
 
-    if(undistorter->photometricUndist != 0)
-    	fullSystem->setGammaFunction(undistorter->photometricUndist->getG());
+  ros::spin();
+  for(IOWrap::Output3DWrapper* ow : fullSystem->outputWrapper)
+  {
+      ow->join();
+      delete ow;
+  }
 
-    ros::NodeHandle nh;
-    ros::Subscriber imgSub = nh.subscribe("image", 1, &vidCb);
-
-    ros::spin();
-    fullSystem->printResult(saveFile); 
-    for(IOWrap::Output3DWrapper* ow : fullSystem->outputWrapper)
-    {
-        ow->join();
-        delete ow;
-    }
-
-    delete undistorter;
-    delete fullSystem;
+  delete undistorter;
+  delete fullSystem;
 
 	return 0;
 }
